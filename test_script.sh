0e2312b4cd4e62eb9d9c4dfc1ec50cbb3eb4a786
hemanth22 <hemanth22hemu@gmail.com>
2025-09-21 03:18:11 +0000

    feat: addig test script

---
+++ test_script.sh
@@ -0,0 +1,8 @@
+python3.13 fastapi_timesheet.py
+python3.13 fastapi_soul.py
+python3.13 fastapi_notify_water.py
+python3.13 fastapi_notify.py
+python3.13 fastapi_CapitalMarket.py
+python3.13 fastapi_stockedge.py
+python3.13 fastapi_valuepickr.py
+python3.13 fastapi_multipe.py
